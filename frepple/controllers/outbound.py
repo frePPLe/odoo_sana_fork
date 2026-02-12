4345fe75c69c379be961c5f347ef69643b8458a1
hichamlahlou <hlahlou@frepple.com>
2026-02-12 12:52:01 +0100

    fix the sequence being the same for 2 suppliers

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -1031,6 +1031,7 @@ class exporter(object):
             "product.supplierinfo",
             fields=supplierinfo_fields,
             search=[("product_tmpl_id", "!=", False)],
+            order="sequence",
         ):
             if i["product_tmpl_id"][0] in itemsuppliers:
                 itemsuppliers[i["product_tmpl_id"][0]].append(i)
@@ -1140,7 +1141,9 @@ class exporter(object):
             # Export suppliers for the item, if the item is allowed to be purchased
             if tmpl["purchase_ok"]:
                 suppliers = {}
+                sequence = 0
                 for sup in itemsuppliers.get(tmpl["id"], []):
+                    sequence += 1
                     name = self.map_suppliers.get(sup["partner_id"][0], None)
                     if not name:
                         # Skip uninterested suppliers (eg archived ones)
@@ -1152,7 +1155,7 @@ class exporter(object):
                             {
                                 "name": name,
                                 "delay": sup["delay"],
-                                "priority": sup["sequence"] or 1,
+                                "priority": sequence,
                                 "size_minimum": sup["min_qty"],
                             }
                         )
@@ -1165,10 +1168,6 @@ class exporter(object):
                             not r["delay"] or sup["delay"] < r["delay"]
                         ):
                             r["delay"] = sup["delay"]
-                        if sup["sequence"] and (
-                            not r["sequence"] or sup["sequence"] < r["sequence"]
-                        ):
-                            r["sequence"] = sup["sequence"]
                         if sup["batching_window"] and (
                             not r["batching_window"]
                             or sup["batching_window"] > r["batching_window"]
@@ -1189,7 +1188,7 @@ class exporter(object):
                     else:
                         suppliers[(name, sup["date_start"])] = {
                             "delay": sup["delay"],
-                            "sequence": sup["sequence"] or 1,
+                            "sequence": sequence,
                             "batching_window": sup["batching_window"] or 0,
                             "min_qty": sup["min_qty"],
                             "price": max(0, sup["price"]),
