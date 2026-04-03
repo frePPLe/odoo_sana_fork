5ce953f3964f7fd64cbc69db8207ea049e97097d
Johan De Taeye <jdetaeye@frepple.com>
2026-04-03 09:41:32 +0200

    bug fix on filtering out outdated suppliers

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -1240,7 +1240,7 @@ class exporter(object):
                 if suppliers:
                     yield "<itemsuppliers>\n"
                     for k, v in suppliers.items():
-                        if v["date_end"] and v["date_end"] < self.currentdate:
+                        if v["date_end"] and v["date_end"] < self.currentdate.date():
                             continue
                         yield '<itemsupplier leadtime="P%dD" priority="%s" batchwindow="P%dD" size_minimum="%f" size_multiple="%f" cost="%f"%s%s><supplier name=%s/></itemsupplier>\n' % (
                             v["delay"],
